d007e634ca11dea4b5917648e537869daf7a4338
Thales Minoru Sato <thalesms2@outlook.com>
2022-03-07 15:52:47 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+//get all keys
+const keys = document.querySelectorAll(".key")
+
+// play notes
+const playNote = (event) => {
+    let audioKeyCode = getKeyCode(event)
+
+    // typed or pressed key
+    const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
+
+    // if key exists
+    const cantFoundAnyKey = !key
+
+    if(cantFoundAnyKey) {
+        return
+    }
+    
+    playAudio(audioKeyCode)
+    
+}
+
+const getKeyCode = (event) => {
+    let keyCode
+    const isKeyboard = event.type === "keydown"
+    if(isKeyboard) {
+        keyCode = event.keyCode
+    } else {
+        keyCode = event.target.dataset.key
+    }
+    return keyCode
+}
+
+const playAudio = (audioKeyCode) => {
+    const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`)
+    audio.currentTime = 0
+    audio.play()
+}
+
+//click with mouse
+keys.forEach((key) => {
+    key.addEventListener("click", playNote)
+})
+// keyboard type
+window.addEventListener("keydown", playNote)
